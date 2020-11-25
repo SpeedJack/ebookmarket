95d886db55633f9a1f609a08ffeed3758625f4cb
Nicola Ferrante <nicolaferrante96@gmail.com>
2020-11-26 00:40:18 +0100

    Added login and session/cookie management

--- src/Entity/AuthToken.php
+++ src/Entity/AuthToken.php
@@ -1,14 +1,42 @@
 <?php
 namespace EbookMarket\Entity;
 
+use DateTime;
+use \EbookMarket\Entity\User;
+
 class AuthToken extends AbstractEntity
 {
     private $userId;
     private $expiration;
+    private $user;
 
-    public function __construct(string $id, $userId, $expiration){
-        parent::__construct($id);
+    public function __construct(string $authToken, int $userId, DateTime $expiration, User $user){
+        parent::__construct($authToken);
         $this->userId = $userId;
         $this->expiration = $expiration;
+        $this->user = $user;
     }
+
+    public function isExpired()
+	{
+		return $this->expiration <= time();
+	}
+
+	public function resetExpireTime()
+	{
+		$this->_set('expireTime', time() + $this->app->config['auth_token_duration']);
+    }
+    
+    public function verifyToken($token)
+	{
+		return password_verify($token, $this->_authToken);
+	}
+
+    public function authenticate($token)
+	{
+		if ($this->isExpired() || !$this->verifyToken($token))
+			return false;
+		$this->resetExpireTime();
+		return $this->_user;
+	}
 }
--- src/Visitor.php
+++ src/Visitor.php
@@ -3,6 +3,8 @@
 declare(strict_types=1);
 
 namespace EbookMarket;
+use \EbookMarket\Entity\User;
+use \EbookMarket\Entity\AuthToken;
 
 class Visitor extends AbstractSingleton
 {
@@ -22,9 +24,15 @@ class Visitor extends AbstractSingleton
 	protected $getParams = [];
 	protected $postParams = [];
 
+	public $user;
+	public $authToken;
+
+
 	protected function __construct()
 	{
 		$this->readParams();
+		$this->initSession();
+		$this->login();
 	}
 
 	public function clearParams(): void
@@ -167,5 +175,124 @@ class Visitor extends AbstractSingleton
 		foreach ($_POST as $key => $value)
 			if (preg_match('/^[A-Za-z_][A-Za-z0-9_]{0,20}$/', $key) === 1)
 				$this->addPostParams([$key => $value]);
+	}
+
+	public function setAuthTokenCookie($authToken, $validator)
+	{
+		$this->setCookie('authToken', [
+				'id' => $authToken->getId(),
+				'validator' => $validator
+			], $authToken->getExpireTime(), true);
+		$this->authToken = $authToken;
+		$this->user = $authToken->getUser();
+	}
+
+	public function setSessionUser($user)
+	{
+		$this->user = $user;
+		$this->initSession();
+		$_SESSION['userid'] = $user->getId();
+	}
+
+	public function session($key)
+	{
+		return isset($_SESSION[$key]) ? $_SESSION[$key] : null;
+	}
+
+	public function cookie($key)
+	{
+		return isset($_COOKIE[$key])
+			? $_COOKIE[$key] : null;
+	}
+
+	public function setCookie($key, $value, $expire = 0, $httponly = false, $path = '/')
+	{
+		if (is_array($value))
+			foreach ($value as $arraykey => $data)
+				setcookie($key . '[' . $arraykey . ']', $data,
+					$expire, $path, $this->_app->serverName,
+					$this->_app->isHttps, $httponly);
+		else
+			setcookie($key, $value, $expire, $path,
+				$this->_app->serverName, $this->_app->isHttps,
+				$httponly);
+		$_COOKIE[$key] = $value;
+	}
+
+	public function unsetCookie($key)
+	{
+		if (!isset($_COOKIE[$key]))
+			return;
+		if (is_array($_COOKIE[$key])) {
+			foreach ($_COOKIE[$key] as &$cookie)
+				$cookie = '';
+			$this->setCookie($key, $_COOKIE[$key], time() - 60*60*24);
+			return;
+		}
+		$this->setCookie($key, '', time() - 60*60*24);
+		unset($_COOKIE[$key]);
+	}
+
+	private function initSession()
+	{
+		if (session_status() === PHP_SESSION_NONE)
+			session_start();
+		if (!isset($_SESSION['canary']) || $_SESSION['canary'] <
+			time() - $this->app->config['session_canary_lifetime']) {
+			session_regenerate_id(true);
+			$_SESSION['canary'] = time();
+			$_COOKIE[session_name()] = session_id();
+		}
+	}
+	
+	private function destroySession()
+	{
+		if (session_status() !== PHP_SESSION_ACTIVE)
+			return;
+		$this->unsetCookie(session_name());
+		session_destroy();
+	}
+
+	private function login()
+	{
+		$user = false;
+		$authToken = false;
+		$userid = $this->session('userid');
+		$clientToken = $this->cookie('authToken');
+		if (!empty($clientToken) && !empty($clientToken['id'])) {
+			$authToken = AuthToken::getById($clientToken['id']);
+			if ($authToken !== false && !empty($clientToken['validator']))
+				$user = $authToken->authenticate($clientToken['validator']);
+			if ($user === false
+				|| (!empty($userid) && $user->getId() !== $userid)) {
+				$authToken = false;
+				$user = false;
+				$this->destroySession();
+				$this->unsetCookie('authToken');
+			} else {
+				$_SESSION['userid'] = $user->getId();
+			}
+		} else if (!empty($userid)) {
+			$user = $this->_em->getFromDb('User', $userid);
+		}
+		$this->user = $user ?: null;
+		$this->authToken = $authToken ?: null;
+		if (!$this->isLoggedIn())
+			$this->logout();
+	}
+
+	public function isLoggedIn()
+	{
+		return isset($this->user);
+	}
+
+	public function logout()
+	{
+		if (isset($this->authToken))
+			$this->authToken->delete();
+		$this->authToken = null;
+		$this->user = null;
+		$this->destroySession();
+		$this->unsetCookie('authToken');
 	}
 }
