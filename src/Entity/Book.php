aaa468d00daf861eac28aea8d5c66f0e1e40921b
Nicola Ferrante <nicolaferrante96@gmail.com>
2020-11-25 22:44:36 +0100

    added price

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -8,13 +8,15 @@ class Book extends AbstractEntity
     private $title;
     private $author;
     private $category;
+    private $price;
     private $date;
 
-    function __construct(int $id, string $title, string $author, Category $category, string $date){
+    function __construct(int $id, string $title, string $author, Category $category, float $price, string $date){
         parent::__construct($id);
         $this->$title = $title;
         $this->$author = $author;
         $this->$category = $category;
         $this->$date = $date;
+        $this->$price = $price;
     }
 }
