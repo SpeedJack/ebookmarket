b05a21e4e121f101b2e9cc2c944a97b57aee78c4
Nicola Ferrante <nicolaferrante96@gmail.com>
2020-11-26 00:15:51 +0100

    Changed navbar to sidebar

--- src/templates/users/profile.php
+++ src/templates/users/profile.php
@@ -2,5 +2,5 @@
 
 <main>
     <h1><?= __($user->username . '\'s Profile') ?><h1>
-    <?php $this->loadTemplate("users/navbar", array($user)); ?>        
+    <?php $this->loadTemplate("users/sidebar", array($user)); ?>        
 </main>
--- src/templates/users/sidebar.php
+++ src/templates/users/sidebar.php
@@ -1,6 +1,8 @@
-<nav>
+<aside>
     <ul>
         <li><?= __('Modify Password') ?></li>
         <li><?= __('Show my ebooks') ?></li>
+        <li><?= __('Active Sessions') ?></li>
+        <li><?= __('Logout') ?></li>
     </ul>
-</nav>
+</aside>
