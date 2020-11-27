8d31803444b630572c4213f5e665f0b30210843b
LCima <l.cima@studenti.unipi.it>
2020-11-27 15:50:05 +0100

    Create tabelle db

--- db.sql
+++ db.sql
@@ -1,13 +1,76 @@
+Drop SCHEMA IF EXISTS ebookmarket;
+CREATE SCHEMA ebookmarket ;
+
 USE ebookmarket;
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
 	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(32) NOT NULL,
-	`email` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(50) NOT NULL,
 	`passwordhash` VARCHAR(255) NOT NULL,
 	`valid` TINYINT NOT NULL DEFAULT '0',
 	PRIMARY KEY (`id`),
-	UNIQUE KEY `username` (`username`),
-	UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+	UNIQUE KEY username (`username`),
+	UNIQUE KEY email (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE `category` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(30) NOT NULL,
+	PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE `book` (
+	`id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(50) NOT NULL,
+	`author` VARCHAR(30) NOT NULL,
+	`date` INTEGER NOT NULL,
+	`price` DECIMAL(10,2) NOT NULL,
+    `category` INT UNSIGNED NOT NULL,
+	PRIMARY KEY (`id`) , 
+    
+    CONSTRAINT `category`
+    FOREIGN KEY (`category`)
+    REFERENCES `ebookmarket`.`category` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+	`book` INT UNSIGNED NOT NULL,
+    `user` INT UNSIGNED NOT NULL,
+	`date` INTEGER NOT NULL,
+	`payment_ok` TINYINT NOT NULL DEFAULT '0',
+	PRIMARY KEY (`book`, `user`), 
+    
+    CONSTRAINT `book`
+    FOREIGN KEY (`book`)
+    REFERENCES `ebookmarket`.`book` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE,
+	
+    CONSTRAINT `user1`
+    FOREIGN KEY (`user`)
+    REFERENCES `ebookmarket`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `auth_token`;
+CREATE TABLE `auth_token` (
+	`id` VARCHAR(32) NOT NULL,
+	`expire_time` INTEGER NOT NULL,
+	`type` ENUM ('PASSWORD_RECOVERY', 'AUTHENTICATION', 'VERIFY_MAIL') NOT NULL,
+    `user` INT UNSIGNED NOT NULL,
+	PRIMARY KEY (`id`) , 
+    
+    CONSTRAINT `user2`
+    FOREIGN KEY (`user`)
+    REFERENCES `ebookmarket`.`user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
