07c55413a4abf3f1bfcce89a5dbcf2ce53cf816b
Niccolò Scatena <speedjack95@gmail.com>
2020-11-28 00:04:49 +0100

    Code restyle

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -1,36 +1,37 @@
 <?php
 
-namespace EbookMarket\Entity;
+declare(strict_types=1);
 
-use EbookMarket\Entity\Book;
-use EbookMarket\Entity\User;
+namespace EbookMarket\Entity;
 
 class Order extends AbstractEntity
 {
-    public static function getStructure(): array
-    {
-        return [
-            'table' => 'order',
-            'columns' => [
-                'id' => ['type' => self::UINT, 'auto_increment' => true],
-                'user' => ['type' => self::STR, 'required' => true],
-                'book' => ['type' => self::STR, 'required' => true],
-                'payment_ok' => ['type' => self::BOOL, 'required' => true, 'default' => 'false'],
-                'date' => ['type' => self::UINT, 'default' => time()]
-            ]
-        ];
-    }
+	public static function getStructure(): array
+	{
+		return [
+			'table' => 'order',
+			'columns' => [
+				'id' => ['type' => self::UINT, 'auto_increment' => true],
+				'user' => ['type' => self::STR, 'required' => true],
+				'book' => ['type' => self::STR, 'required' => true],
+				'payment_ok' => ['type' => self::BOOL, 'required' => true, 'default' => 'false'],
+				'date' => ['type' => self::UINT, 'default' => time()]
+			]
+		];
+	}
 
 
-    public function pay(string $cc_number, string $cc_cv2, float $amount): bool
-    {
-        $bookToBuy = Book::get($this->book);
-        if($bookToBuy == null) return false; 
-        if($amount != $bookToBuy->price) return false;
-        $this->payment_ok = true;
-        $this->save();
-        //if(!validate($cc_number) || !validate($cc_cv2)) return false;
-        //if(!payment($cc_number, $cc_cv2, $amount)->success) return false;
-        return true;
-    }
+	public function pay(string $cc_number, string $cc_cv2, float $amount): bool
+	{
+		$bookToBuy = Book::get($this->book);
+		if($bookToBuy == null)
+			return false;
+		if($amount != $bookToBuy->price)
+			return false;
+		$this->payment_ok = true;
+		$this->save();
+		//if(!validate($cc_number) || !validate($cc_cv2)) return false;
+		//if(!payment($cc_number, $cc_cv2, $amount)->success) return false;
+		return true;
+	}
 }
