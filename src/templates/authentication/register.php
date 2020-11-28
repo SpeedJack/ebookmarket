e2474e90c55cbbefffdeceb29e72a9153ee18e8e
Nicola Ferrante <nicolaferrante96@gmail.com>
2020-11-28 16:28:01 +0100

    added action and method

--- src/templates/authentication/register.php
+++ src/templates/authentication/register.php
@@ -1,6 +1,6 @@
 <main>
     <h1><?= __('Register') ?></h1>
-    <form>
+    <form action =  "<?php $app->buildLink("auth/register") ?>" method = "post">
         <label for="username"><?= __('Username') ?></label>
         <input type="text" name="username" />
         <label for="email"><?= __('Email') ?></label>
@@ -8,8 +8,8 @@
         <label for="password"><?= __('Password') ?></label>
         <input type="password" name="password" />
         <label for="password_confirm"><?= __('Confirm password') ?></label>
-        <input type="password_confirm" />
-        <label for="accpet_terms"><?= __('Accept terms and conditions') ?></label>
+        <input type="password" name = "password_confirm" />
+        <label for="accept_terms"><?= __('Accept terms and conditions') ?></label>
         <input type="checkbox" name="accept_terms" />
         <button type="submit"><?= __('Register') ?></button>
     </form>
