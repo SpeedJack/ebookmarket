09d803e5e8dd44b1dd0331deb3a43e8b84202abf
Nicola Ferrante <nicolaferrante96@gmail.com>
2020-12-02 17:04:46 +0100

    added logout template

---
+++ src/templates/account/logout.php
@@ -0,0 +1,3 @@
+<h1>Logout <?= $success ? "Complete!" : "Failed!"?></h1>
+<p><a href="<?= $app->buildLink("/login") ?>">Click here to Login</a></p>
+<a href="<?= $app->buildLink("/") ?>">Click here to Return to HomePage</a></p>
